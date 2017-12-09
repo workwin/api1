39d04c7acac52da4e7eb99d78bba77c047f928be
mypassioniswork <neha.genious@gmail.com>
2017-12-09 23:21:19 +0530

    do

--- dbcon.php
+++
@@ -1,5 +0,0 @@
-<?php
-//$conn=mysqli_connect("localhost","","") or die("sorry! No server not found");
-//$select_db=mysqli_select_db("", $conn) or die("database not selected...!!!");
-$conn=mysqli_connect("localhost","","","ndia_video")or die("sorry! No server not found");
-?>
